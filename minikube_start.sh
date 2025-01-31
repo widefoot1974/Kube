a42e4e62982a89dbfaadb4cd36a94b3d3784d083
widefoot1974 <widefoot1974@naver.com>
2025-02-01 07:17:17 +0900

    Update minikube_start.sh: add 'k taint ~~'

--- minikube_start.sh
+++ minikube_start.sh
@@ -2,3 +2,6 @@
 
 # minikube start
 minikube start --driver=podman -n 3 --insecure-registry="10.10.9.11:8080"
+
+# master node no schedule
+kubectl taint nodes minikube node-role.kubernetes.io/control-plane=:NoSchedule
